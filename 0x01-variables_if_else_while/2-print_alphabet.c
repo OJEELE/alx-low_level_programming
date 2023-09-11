987f657c656f8c92bf8098e8a72ce35ffef60057
OJEELE <ojeelesam@gmail.com>
2023-09-11 09:02:36 -0700

    code prints alphabetsin lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
@@ -10,7 +8,7 @@
 
 int main(void)
 {
-	char low;
+	char low = 'a';
 
 	while (low <= 'z')
 	{
