9b88ddd7843ff30e259f147e22b66b7e88e49dfa
OJEELE <ojeelesam@gmail.com>
2023-09-13 10:36:57 -0700

    a program that prints _putchar.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - Check description
+ * Description: It prints the word _putchar, followed by a new line.
+ * Return: 0.
+ */
+int main(void)
+{
+	char word[8] = "_putchar";
+	int j;
+
+	for (j = 0; j <= 8; j++)
+		_putchar(word[j]);
+	_putchar('\n');
+	return (0);
+}
