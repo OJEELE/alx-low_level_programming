58dd546b537a4e3b4aa40ad61d22f51ecd546859
OJEELE <ojeelesam@gmail.com>
2023-09-20 15:23:25 -0700

    a function that concatenates two strings.
        Prototype: char *_strcat(char *dest, char *src);

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,14 @@
-#ifndef _MAIN_H
+#ifndef  _MAIN_H
 #define _MAIN_H
-char *_strncat(char *dest, char *src, int n)
-char *_strncpy(char *dest, char *src, int n)
-int _strcmp(char *s1, char *s2)
-void reverse_array(int *a, int n)
-char *string_toupper(char *)
-char *cap_string(char *)
-#endif 
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *s);
+char *cap_string(char *s);
+char *leet(char *s);
+char *rot13(char *);
+void print_number(int n);
+#endif
