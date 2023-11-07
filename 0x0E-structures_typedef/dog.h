494586cd23b889e1c45efaf05811253ac9ce0150
ojeele <ojeelesam@gmail.com>
2023-11-07 07:08:17 -0800

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog- typedf struct for dog
+ * @name: string element
+ * @age: float element
+ * @owner: string pointer to owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*_DOG_H */
