62bdee5d734dec23040b50c0fe62d68e7f99a606
ojeele <ojeelesam@gmail.com>
2023-11-21 06:36:49 -0800

    file update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,10 +9,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	for (count = 0; h; count++)
+	while (h)
 	{
 		printf("%d\n", h->n);
-	h = h->next;
+		h = h->next;
+		count++;
 	}
 	return (count);
 }
