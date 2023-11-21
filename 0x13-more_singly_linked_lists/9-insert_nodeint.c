4de25159d72bbfd5c052677f1df385e3ad467249
ojeele <ojeelesam@gmail.com>
2023-11-21 06:59:45 -0800

    file upload

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - function inserts a new node at given position
+ * @idx: index of the list to add new node
+ * @n: integer
+ * Return: address of the new node or NULL if failed
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+
