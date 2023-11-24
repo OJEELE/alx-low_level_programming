e1d7bd1e081aeeda6d2714042b47593772c91a13
ojeele <ojeelesam@gmail.com>
2023-11-24 13:07:26 -0800

    clear bits

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,11 +7,9 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int max = 0x01;
-
-	max = ~(max << index);
-	if (max == 0x00)
+	if (index > (sizeof(*n) * 8))
 		return (-1);
-	*n &= max;
+
+	*n &= ~(1 << index);
 	return (1);
 }
