ab4c5aa9755a77a483ba4a5dd73015e1efbb8b94
ojeele <ojeelesam@gmail.com>
2023-11-24 12:52:19 -0800

    functions prints binary representation of nums

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,22 +7,11 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int flag = 0, max = 32768;
+	unsigned long num = n;
 
-	if (n == 0)
+	if (num > 1)
 	{
-		_putchar ('0');
-		return;
+		print_binary(num >> 1);
 	}
-	while (max)
-	{
-		if (flag == 1 && (n & max) == 0)
-			_putchar('0');
-		else if ((n & max) != 0)
-		{
-			_putchar('1');
-			flag = 1;
-		}
-		max >>= 1;
-	}
+	_putchar((num & 1) + '0');
 }
