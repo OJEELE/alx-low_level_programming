dbd58fe4ec92f9fc9a0ba868a79079307a6146a4
root <ojeelesam@gmail.com>
2024-02-13 22:04:35 -0800

    fixed creat file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,24 +1,38 @@
 #include "main.h"
+
 /**
- * create_file - function creates file
- * @filename: file to create
- * @text_content: NULL terminated string to write to file
- * Return: 1 on success, -1 on failure
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fop, len = 0, fwr;
+	int fd;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	fop = open(filename, O_CREAT | O_WRONLY | O_TRUNC | 0600);
-	if (fop < 0)
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
 		return (-1);
-	while (text_content && text_content[len])
-		len++;
-	fwr = write(fop, text_content, len);
-	if (fwr < 0)
+
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
 		return (-1);
-	close(fop);
+
+	close(fd);
+
 	return (1);
 }
