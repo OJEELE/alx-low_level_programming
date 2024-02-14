ce02b2c3f0014ddf8a7c17ef401dca375b4d2bff
root <ojeelesam@gmail.com>
2024-02-14 02:07:54 -0800

    append fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, err, len = 0;
 
-	if (filename == NULL || text_content == NULL || text_content[0] =='\0')
+	if (filename == NULL || text_content == NULL || text_content[0] == '\0')
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd < 0)
