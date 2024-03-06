4c486b9d59c825d5681a5e2059cfc0ca3ca50035
root <ojeelesam@gmail.com>
2024-03-06 05:05:25 -0800

    appent text update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,18 +10,30 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, err, len = 0;
+	int fd;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL || text_content == NULL || text_content[0] == '\0')
+	if (!filename)
 		return (-1);
+
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd < 0)
+
+	if (fd == -1)
 		return (-1);
-	while (text_content[len])
-		len++;
-	err = write(fd, text_content, len);
-	if (err < 0)
-		return (-1);
+
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+
+		rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
+	}
+
 	close(fd);
+
 	return (1);
 }
